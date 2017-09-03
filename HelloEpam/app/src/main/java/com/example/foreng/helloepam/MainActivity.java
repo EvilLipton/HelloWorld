eff49df4642605609a537c985df8c91472bcd13c
EvilLipton <beloguzov-ilya@mail.ru>
2017-09-03 23:41:02 +0300

    Finished the assignment
    I added a toast

--- HelloEpam/app/src/main/java/com/example/foreng/helloepam/MainActivity.java
+++ HelloEpam/app/src/main/java/com/example/foreng/helloepam/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.foreng.helloepam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static com.example.foreng.helloepam.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_main = (Button) findViewById(R.id.button);
+
+        button_main.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(android.view.View view) {
+                Toast.makeText(MainActivity.this, R.string.toast_name, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
     }
 }
